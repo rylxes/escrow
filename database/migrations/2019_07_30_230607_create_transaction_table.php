07cfefdf7a7cfb5940d94ac07d294a7c302d7044
sherriff <sherriff.agboola@ikooba.com>
2019-07-31 00:14:00 +0100

    first commit added migrations

---
+++ database/migrations/2019_07_30_230607_create_transaction_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('price');
+            $table->string('sender_email');
+            $table->string('sender_phone');
+            $table->string('recipient_phone');
+            $table->string('recipient_email');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction');
+    }
+}
